f86c8741b5d1c9b0b92d7b7242e6e9dd77165a1d
lizzybeliy0 <lizibeliy.0@mail.ru>
2025-10-11 07:34:12 +0300

    fix: add jsons for ts

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.use(express.static(path.resolve(__dirname, '..', 'node_modules')));
+app.use('/dist', express.static(path.resolve(__dirname, '..', 'dist')));
 
 
 app.use(bodyParser.json());
